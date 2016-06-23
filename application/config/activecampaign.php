85a0722a6375c87edb8cb254bed78dcd8484b28d
Cosmin <cosmin.pascu@cbsinteractive.com>
2016-06-22 21:21:09 -0700

    Added Activecampaign config

---
+++ application/config/activecampaign.php
@@ -0,0 +1,10 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+/**
+ * Config used in the activecampaign wrapper CI library.
+ *
+ * @author  Cosmin Pascu <csmnpsc@gmail.com>
+ */
+
+$config['activecampaign_url'] = 'https://codetest.api-us1.com';
+$config['activecampaign_key'] = '225f9d21c7b0fd685f4876dee85950ddb1e645cf9db54e936ca3a51bd115182b376014c3';
