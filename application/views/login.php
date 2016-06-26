3ad756faea4a367a922f08c57ad8ad04742234af
Cosmin <cosmin.pascu@cbsinteractive.com>
2016-06-26 14:44:57 -0700

    Pretty login page

--- application/views/login.php
+++ application/views/login.php
@@ -14,29 +14,53 @@
 	</script>
 </head>
 <body>
-	<button class="js-facebook">Facebook</button>
-	<a href="<?= $githubLogin ?>">Github</a>
-	<?= form_open('api/login', array('class' => 'js-auth')); ?>
-	<div>
-	<?= form_input(array(
-		'name'        => 'username',
-		'class'       => 'js-input js-username',
-		'placeholder' => 'Username'
-	)); ?>
-	</div><div>
-	<?= form_password(array(
-		'name'        => 'password',
-		'class'       => 'js-input js-password',
-		'placeholder' => 'Password'
-	)); ?>
-	</div><div>
-	<?= form_submit(array(
-		'name'  => 'submit',
-		'class' => 'js-submit',
-		'value' => 'Login',
-	)); ?>
+	<div class="container">
+		<div class="row">
+			<div class="col">
+				<h1 class="text-center">Login</h1>
+				<br />
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-xs-offset-3 col-xs-6">
+				<div type="button" class="js-facebook btn btn-primary btn-block">Facebook</div>
+				<br />
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-xs-offset-3 col-xs-6">
+				<a type="button" href="<?= $githubLogin ?>" class="github btn btn-info btn-block">Github</a>
+				<br />
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-xs-offset-3 col-xs-6">
+				<?= form_open('api/login', array('class' => 'js-auth', 'role' => 'form')); ?>
+				<div class="form-group">
+					<?= form_input(array(
+						'name'        => 'username',
+						'class'       => 'js-input js-username form-control',
+						'placeholder' => 'Username'
+					)); ?>
+				</div>
+				<div class="form-group">
+					<?= form_password(array(
+						'name'        => 'password',
+						'class'       => 'js-input js-password form-control',
+						'placeholder' => 'Password'
+					)); ?>
+				</div>
+				<div class="form-group">
+					<?= form_submit(array(
+						'name'  => 'submit',
+						'class' => 'js-submit btn btn-success form-control',
+						'value' => 'Login',
+					)); ?>
+				</div>
+				<?= form_close(); ?>
+			</div>
+		</div>
 	</div>
-	<?= form_close(); ?>
 
 	<script type="text/javascript" src="/assets/bower_components/jquery/dist/jquery.js"></script>
 	<script type="text/javascript" src="/assets/bower_components/bootstrap/dist/js/bootstrap.js"></script>
